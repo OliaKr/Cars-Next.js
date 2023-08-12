bb2f06fa30105bfe4aad6cc8bf18fb6739755a22
OliaK <olgamr@walla.com>
2023-08-12 18:58:49 +0300

    QA

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
